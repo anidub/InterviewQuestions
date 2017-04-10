a34e03ead1879b80a28fd2c635a9688ce9ab1a5f
Anish D <anish_dubey@hotmail.com>
2017-04-09 22:51:05 -0400

    new code

--- src/InterviewBit/HeapsAndMapsMagicianChoclate.java
+++ src/InterviewBit/HeapsAndMapsMagicianChoclate.java
@@ -1,59 +1,57 @@
 package InterviewBit;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.PriorityQueue;
-
-public class HeapsAndMapsMagicianChoclate {
-
-	public static class StringComparator implements Comparator<String>{
-		public int compare(String x, String y){
-			if(x.length() < y.length()) return 1;
-			if(x.length() > y.length()) return -1;
-			return 0;
-		}
-	}
+/*https://www.interviewbit.com/problems/magician-and-chocolates/
+Given N bags, each bag contains Ai chocolates. There is a kid and a magician.
+In one unit of time, kid chooses a random bag i, eats Ai chocolates, then the magician fills the ith bag with floor(Ai/2) chocolates.
+K = 3
+N = 2
+A = 6 5
+	Return: 14
+	At t = 1 kid eats 6 chocolates from bag 0, and the bag gets filled by 3 chocolates
+	At t = 2 kid eats 5 chocolates from bag 1, and the bag gets filled by 2 chocolates
+	At t = 3 kid eats 3 chocolates from bag 0, and the bag gets filled by 1 chocolate
+	so, total number of chocolates eaten: 6 + 5 + 3 = 14*/
+	public class HeapsAndMapsMagicianChoclate {
 	
 	public static void main(String[] args) {
 		int time = 3;
 		ArrayList<Integer> choclates = new ArrayList<Integer>();
 		choclates.add(6);choclates.add(5);
-		//System.out.println(nchoc(time, choclates));
-		
-		Comparator<String> comparator = new StringComparator();
-		PriorityQueue<String> pq = new PriorityQueue<String>(comparator);
-		pq.add("Hello");
-		pq.add("Amazon");
-		pq.add("Allahabad");
-		 while (pq.size() != 0)
-	        {
-	            System.out.println(pq.remove());
-	        }	
-
+		System.out.println(maxChoclates(time, choclates));
 	}
-	//not working
-	static long  mod = 1000000007;
-	public static int nchoc(int time, ArrayList<Integer> choclates) {
-		int N = choclates.size();
-		int K = time;
-		long ans = 0;
-		Collections.reverse(choclates);
-		for(int i = 0; i < choclates.size(); i++){
-			pq.add(choclates.get(i));
+	/*
+	 * we use priority queue to maintain the list in the reverse sorted fashion.
+	 * The max num will always be on top.
+	 */
+	private static class ArrayContainer implements Comparable<ArrayContainer>{
+		int num; 
+		public ArrayContainer(int left){
+			this.num = num;
 		}
-		//priority_queue<Integer> heap(B.begin(),B.end());
-		while(K > 0){
-			long max_elem = pq.peek();
-			ans += max_elem;
-			ans = ans % mod;
-			pq.poll();
-			pq.add((int)(max_elem/2));
-			K--;
-		}   
-		return (int) ans;
-	}
+		
+		public int compareTo(ArrayContainer o){
+			return (new Integer(o.num).compareTo(this.num));
+		}
+	}	
 	
-	
-}
+	public static int maxChoclates(int time, ArrayList<Integer> bags) {
+		if (bags.isEmpty())
+			return 0;
+		int result = 0;
 
+		PriorityQueue<ArrayContainer> pq = new PriorityQueue<ArrayContainer>();
+		for (Integer i : bags) {
+			pq.add(new ArrayContainer(i));
+		}
+		while (time > 0) {
+			ArrayContainer e = pq.poll();
+			int eat = e.num;
+			result += eat;
+			pq.add(new ArrayContainer(eat / 2));
+			time--;
+		}
+		return result;
+	}
+}
