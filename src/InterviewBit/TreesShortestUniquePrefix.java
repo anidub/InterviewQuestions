84fa5e62077422230a2ac8cd164f44eda2f974ec
Anish D <anish_dubey@hotmail.com>
2017-04-16 22:41:59 -0400

    desc

--- src/InterviewBit/TreesShortestUniquePrefix.java
+++ src/InterviewBit/TreesShortestUniquePrefix.java
@@ -1,7 +1,14 @@
 package InterviewBit;
 
 public class TreesShortestUniquePrefix {
-
+/*	Find shortest unique prefix to represent each word in the list.
+	Input: [zebra, dog, duck, dove]
+			Output: {z, dog, du, dov}
+			where we can see that
+			zebra = z
+			dog = dog
+			duck = du
+			dove = dov*/
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
