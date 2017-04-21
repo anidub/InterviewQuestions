527555e02bf4c9dc7e21361bc453adf1f56ecdff
Anish D <anish_dubey@hotmail.com>
2017-04-20 22:36:58 -0400

    new method

--- src/numbers2/rotateNumber.java
+++ src/numbers2/rotateNumber.java
@@ -5,7 +5,7 @@ public class rotateNumber {
 	public static void main(String[] args) {
 		int[] num = {1,2,3,4,5};
 		int rotate = 66;
-		int[] res = rot(num, rotate); // 3 4 5 1 2
+		int[] res = rotate(num, rotate); // 3 4 5 1 2
 		for(int k : res){
 			System.out.print(k + " ");
 		}
@@ -22,12 +22,36 @@ public class rotateNumber {
 		for(int i = 0; i < num.length; i++){
 			temp[k++] = num[i];
 		}
-		k = num.length-1;
+		k = 0;
 		int[] result = new int[num.length];
-		for(int j = temp.length - rotate - 1; j >= 0; j--){
-			result[k--] = temp[j];
-			if(k < 0) break;
+		for(int i = rotate; i < temp.length; i++){
+			if(k  > num.length-1) break;
+			result[k++] = temp[i];
+			
 		}
 		return result;		
+	}
+/*	http://www.programcreek.com/2015/03/rotate-array-in-java/
+		 O(1) space and in O(n) time*/
+	public static int[] rotate(int[] arr, int k){
+		int[] result = new int[arr.length];
+		if(k > arr.length)
+			k = k % arr.length;
+		int arrayBreak = arr.length-k;
+		reverse(arr,result, 0, arrayBreak-1);
+		reverse(arr,result, arrayBreak,arr.length-1);
+		reverse(arr,result, 0,arr.length-1);
+		return result;
+	}
+	
+	public static void reverse(int[] arr,int[] result, int left, int right){
+		if(arr == null || arr.length == 1)
+			return;
+		while(left <= right){
+			int temp = arr[left];
+			result[left] = arr[right];
+			result[right] = temp;
+			left++; right--;			
+		}
 	}
 }
