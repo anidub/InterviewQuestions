25d7e61439f333d790433b194742b82cd60a997d
Anish D <anish_dubey@hotmail.com>
2017-04-23 23:57:04 -0400

    desc

--- src/situations/mincostmaze.java
+++ src/situations/mincostmaze.java
@@ -3,7 +3,13 @@ package situations;
 public class mincostmaze {
 static int R = 3;
 static int C = 3;
-//http://www.geeksforgeeks.org/dynamic-programming-set-6-min-cost-path/
+/*//http://www.geeksforgeeks.org/dynamic-programming-set-6-min-cost-path/
+Given a cost matrix cost[][] and a position (m, n) in cost[][], write a function that returns cost of 
+minimum cost path to reach (m, n) from (0, 0). Each cell of the matrix represents a cost to traverse
+through that cell. Total cost of a path to reach (m, n) is sum of all the costs on that path
+(including both source and destination). You can only traverse down, right and diagonally lower cells from a given cell, 
+i.e., from a given cell (i, j), cells (i+1, j), (i, j+1) and (i+1, j+1) can be traversed. 
+You may assume that all costs are positive integers.*/
 //Time Complexity of the DP implementation is O(mn) 
 	public static void main(String[] args) {
 		int[][] cost = { {1, 2, 3},
@@ -47,6 +53,4 @@ static int C = 3;
 	   else
 	      return (y < z)? y : z;
 	}
-	
-
 }
