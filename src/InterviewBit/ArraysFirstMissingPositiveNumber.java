99b7b5f0695ff8ee137ac11f015131ced3866176
Anish D <anish_dubey@hotmail.com>
2017-05-07 22:00:59 -0400

    new method

--- src/InterviewBit/ArraysFirstMissingPositiveNumber.java
+++ src/InterviewBit/ArraysFirstMissingPositiveNumber.java
@@ -7,17 +7,18 @@ public class ArraysFirstMissingPositiveNumber {
 			[-8, -7, -6] returns 1
 			Your algorithm should run in O(n) time and use constant space.
 			http://n00tc0d3r.blogspot.com/2013/03/find-first-missing-positive.html
+			https://www.interviewbit.com/problems/first-missing-integer/
 */	public static void main(String[] args) {
 		int[] arr = {3,4,-1,1};
-		System.out.println(firstMissingPositive(arr));
-
+		//System.out.println(firstMissingPositive(arr));
+		System.out.println(firstMissingPositiveInt(arr));
 	}
 	
 	 public static int firstMissingPositive(int[] A) {  
 		   // in-place swap (bucket sort)  
 		   int i = 0;  
 		   while (i < A.length) {  
-		     if (A[i] > 0 && A[i] <= A.length && A[i] != i+1 && A[i] != A[A[i]-1]) {  
+		     if (A[i] > 0 && A[i] <= A.length && A[i] != i+1 && A[i] != A[A[i]-1]) {
 		       int temp = A[A[i]-1];  
 		       A[A[i]-1] = A[i];  
 		       A[i] = temp;  
@@ -25,10 +26,42 @@ public class ArraysFirstMissingPositiveNumber {
 		       ++i;  
 		     }  
 		   }  
+		   for(Integer k : A){
+			   System.out.print(k+ " ");
+		   }
 		   // find the first positive missing integer  
 		   i = 0;  
 		   while (i < A.length && A[i] == i+1) 
 			   ++i;  
 		   return i+1;  
 		 }
+	 //http://www.programcreek.com/2014/05/leetcode-first-missing-positive-java/
+	 public static int firstMissingPositiveInt(int[] A) {
+		 for(Integer k : A){
+			   System.out.print(k+ " ");
+		   }
+	        int n = A.length;
+	 
+	    	for (int i = 0; i < n; i++) {
+	    		while (A[i] != i + 1) {
+	    			if (A[i] <= 0 || A[i] >= n)
+	    				break;
+	 
+	                	if(A[i]==A[A[i]-1])
+	                    		break;
+	 
+	    			int temp = A[i];
+	    			A[i] = A[temp - 1];
+	    			A[temp - 1] = temp;
+	    		}
+	    	}
+	 
+	    	for (int i = 0; i < n; i++){
+	    		if (A[i] != i + 1){
+	    			return i + 1;
+	    		}
+	    	}	
+	 
+	    	return n + 1;
+	}
 }
