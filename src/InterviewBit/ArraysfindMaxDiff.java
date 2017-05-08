ef4e035891596300de51a324ce70984f41b28a80
Anish D <anish_dubey@hotmail.com>
2017-05-07 20:33:11 -0400

    desc

--- src/InterviewBit/ArraysfindMaxDiff.java
+++ src/InterviewBit/ArraysfindMaxDiff.java
@@ -1,9 +1,11 @@
+
 package InterviewBit;
 
 public class ArraysfindMaxDiff {
 /*http://www.geeksforgeeks.org/given-an-array-arr-find-the-maximum-j-i-such-that-arrj-arri/
 https://www.interviewbit.com/problems/maximum-consecutive-gap/
 http://stackoverflow.com/questions/18281625/find-the-max-difference-between-j-and-i-indices-such-that-j-i-and-aj-ai
+https://www.youtube.com/watch?v=_3sCTc9BzoY
 Given an array arr[], find the maximum j – i such that arr[j] > arr[i].Time Complexity: O(n)*/
 	public static int maxGap(int[] arr){ //O(N2)
 		if(arr.length < 2) return -1;
@@ -28,11 +30,11 @@ Given an array arr[], find the maximum j – i such that arr[j] > arr[i].Time Comp
 		rMax[l-1] = arr[l-1];
  /* Construct LMin[] such that LMin[i] stores the minimum value from (arr[0], arr[1], ... arr[i]) */		
 		for(i = 1; i < l; i++){
-			lMin[i] = Math.min(arr[i],arr[i-1]);
+			lMin[i] = Math.min(arr[i],lMin[i-1]);
 		}
 /* Construct RMax[] such that RMax[j] stores the maximum value from (arr[j], arr[j+1], ..arr[n-1]) */	
 		for(j = l-2; j >= 0; j--){
-			rMax[j] = Math.max(arr[j],arr[j+1]);
+			rMax[j] = Math.max(arr[j],rMax[j+1]);
 		}
 		  /* Traverse both arrays from left to right to find optimum j - i*/
 		i = 0; j = 0;maxDiff=0;
@@ -43,9 +45,9 @@ Given an array arr[], find the maximum j – i such that arr[j] > arr[i].Time Comp
 			}else
 				i = i+1;
 		}
-		return maxDiff;
-			
+		return maxDiff;			
 	}
+	
 	public static void main(String[] args) {
 		int[] arr= {34, 8, 10, 3, 2, 80, 30, 33, 1};
 		System.out.println(findMaxGap(arr));
