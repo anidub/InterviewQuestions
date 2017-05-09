b11cace4133a37a451365773460d6e72f777e15b
Anish D <anish_dubey@hotmail.com>
2017-05-09 01:10:50 -0400

    last word length new method

--- src/InterviewBit/StringFindLengthLastWord.java
+++ src/InterviewBit/StringFindLengthLastWord.java
@@ -6,23 +6,29 @@ public class StringFindLengthLastWord {
 If the last word does not exist, return 0.Given s = "Hello World",return 5 as length("World") = 5.*/
 //or can be framed like implement splitter for strings on space	
 	public static void main(String[] args) {
-		System.out.println(getLastLength("Wordl  "));
-	}
-	
-	public static int getLastLength(String a){ //O(N)
-		if(a.length() == 0) return 0; 
-		if(a.length() == 1) return 1;
-		StringBuilder sb = new StringBuilder();
-		int i = a.length()-1;
-		while(a.charAt(i) != ' ' || i > -1){
-				if(a.charAt(i) != ' '){
-					sb.append(a.charAt(i));
-					}
-				i--;
-				if(i < 0)break;
+		System.out.println(getlast("Hello Wordl  "));
+	}	
+	public static int getlast(String s){
+		if(s == null || s.length() == 0)return 0;
+		if(s.length() == 1 && s.charAt(0) == ' ') return 0;
+		int i = s.length()-1;
+		while(i >= 0 && s.charAt(i) == ' '){
+			i--;
 		}
-		if(sb.length() > 0) return sb.length();
-		else return 0;
+		StringBuilder sb = new StringBuilder();
+		int space = 1;
+		while(i > -1){
+			char c = s.charAt(i);
+			if(s.charAt(i) == ' '){
+				space++;
+			}
+			if(space > 1) break;
+			sb.append(s.charAt(i));
+			i--;
+		}
+		if(sb.length() > 0)		
+		return sb.toString().length();
 		
+		return 0;
 	}
 }
