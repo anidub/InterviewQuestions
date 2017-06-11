4879fd07a5ba876d2d62ff92afaba031833feb3e
Anish D <anish_dubey@hotmail.com>
2017-06-11 12:36:40 -0400

    new file

---
+++ src/Strings2/removeArbitarySpace.java
@@ -0,0 +1,29 @@
+package Strings2;
+
+public class removeArbitarySpace {
+
+	//bloomberg interview
+	/*
+	 * Remove arbitary spaces from a sentence
+	 */
+	public static void main(String[] args) {
+		remove();
+	}
+	
+	public static void remove(){
+		String s = "The sky    is   blue";
+		StringBuilder sb = new StringBuilder();
+		boolean  isSpace = true;
+		for(int i = 0; i < s.length(); i++){
+			if(s.charAt(i) != ' '){
+				sb.append(s.charAt(i));
+				isSpace = false;
+			}else if(!isSpace){
+				isSpace = true;
+				sb.append(" ");
+			}
+		}		
+		System.out.println(sb.toString());
+	}
+
+}
