e8d3961e301f15a35e410e9f94737ee1f29ccd6c
Anish D <anish_dubey@hotmail.com>
2017-07-19 00:07:28 -0400

    desc

--- src/Strings2/sumofallnumberspresentinstring.java
+++ src/Strings2/sumofallnumberspresentinstring.java
@@ -1,8 +1,7 @@
 package Strings2;
 
+import java.util.ArrayList;
 import java.util.HashMap;
-
-import org.xml.sax.HandlerBase;
 
 public class sumofallnumberspresentinstring {
 /*//http://www.geeksforgeeks.org/calculate-sum-of-all-numbers-present-in-a-string/
@@ -12,7 +11,7 @@ public class sumofallnumberspresentinstring {
 	Output: 24*/
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		cal("");
 	}
 	
 	public static int calsum(String s){
@@ -35,20 +34,72 @@ public class sumofallnumberspresentinstring {
 	}
 	
 	public static int cal(String s){
+		//correct answer
+		String inputStr = "xyzonexyztwothreeeabrminussevenskdjjtwozerodone";
+        int i=0, sum=0;
+        String currString = "";
+        
+        inputStr =inputStr.replace("one", "1");
+        inputStr =inputStr.replace("two", "2");
+        inputStr =inputStr.replace("three", "3");
+        inputStr =inputStr.replace("four", "4");
+        inputStr =inputStr.replace("five", "5");
+        inputStr =inputStr.replace("six", "6");
+        inputStr =inputStr.replace("seven", "7");
+        inputStr =inputStr.replace("eight", "8");
+        inputStr =inputStr.replace("nine", "9");
+        inputStr =inputStr.replace("zero", "0");
+        inputStr =inputStr.replace("minus", "-");
+        
+        while(i<inputStr.length()) {
+			if (inputStr.charAt(i) >= '0' && inputStr.charAt(i) <= '9' || inputStr.charAt(i) == '-') {
+				currString = currString + inputStr.charAt(i);
+				i++;
+			} else {
+				if (currString != "") {
+					sum = sum + Integer.parseInt(currString);
+					currString = "";
+				}
+				i++;
+			}
+		}
+        
+        if(currString != "")
+            sum = sum + Integer.parseInt(currString);
+        
+         System.out.println(sum);
+         return -1;
+		/*
+		 * half answer
 		HashMap<String, String> map= new HashMap<String, String>();
 		map.put("zero", "0");
 		map.put("one", "1");
 		map.put("two", "2");
 		map.put("three", "3");
-		
-		StringBuilder sb = new StringBuilder();
+		map.put("four", "4");
+		s = "xyzoneyctwothreefourvone";
 		int temp;
+		StringBuilder strB = new StringBuilder();
+		ArrayList<StringBuilder> list = new ArrayList<StringBuilder>();
+		StringBuilder strB1 = new StringBuilder();
+		boolean isNum = false;
+
+		for (int i = 0; i <= s.length() - 1; i++) {
+			strB.append(s.charAt(i));
+			if (strB.length() >= 3 && map.containsKey(strB.toString())) {
+					isNum = true;
+					strB1.append(map.get(strB.toString().toLowerCase()));
+					strB = new StringBuilder();
+				} else if (strB.length() > 5) {
+					isNum = false;
+					strB1.append("+");
+					strB = new StringBuilder();
+					i = i - 5;
+				}
+			}
 		
-		for(int i = 0; i < s.length(); i++){
-			
-			
-		}
-		return 0;
 		
-	}
+		System.out.println(strB1.toString());
+		System.out.println("------");		
+	*/}
 }
