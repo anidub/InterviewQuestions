802ed83ec67f94fea849be1dd49a35a3455ec345
Anish D <anish_dubey@hotmail.com>
2017-08-09 00:49:58 -0400

    new

--- src/Strings/firstnonrepeatedchar.java
+++ src/Strings/firstnonrepeatedchar.java
@@ -1,20 +1,21 @@
 package Strings;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.LinkedHashMap;
 
 public class firstnonrepeatedchar {
 
 	public static void main(String[] args) {
 		String s = "Helo How are you ? ";
-		Set<Character> stringmap = new HashSet<Character>();
+		LinkedHashMap<Character, Integer> map = new LinkedHashMap<Character, Integer>();
 		for(int i = 0; i < s.length(); i++){
-			if(stringmap.contains(s.charAt(i))){
-				System.out.println(s.charAt(i));
-				break;
-			}else
-				 stringmap.add(s.charAt(i));
+			char c = s.charAt(i);
+			map.put(c, map.getOrDefault(c, 0)+1);
+		}
+		for(char c : map.keySet()){
+			if(map.get(c) == 1){
+				System.out.println(c);
+				return;
+			}
 		}
 	}
-
 }
