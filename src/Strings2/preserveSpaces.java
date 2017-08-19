9d2c33fccac51b2d14e06e1e7963ddfb2d2eb2cd
Anish D <anish_dubey@hotmail.com>
2017-08-19 00:19:41 -0400

    new

---
+++ src/Strings2/preserveSpaces.java
@@ -0,0 +1,37 @@
+package Strings2;
+
+public class preserveSpaces {
+///preserve spaces where they are and reverse words
+	public static void main(String[] args) {
+		String orig = "    Hello   World  I  a";
+		System.out.println(preserveSpaces(orig));
+	}
+	
+	public static String preserveSpaces(String input){
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < input.length(); i++){
+			char c = input.charAt(i);
+			if(input.charAt(i) == ' '){
+				sb.append(" ");
+			}else{
+				int endIndex = i;
+				while(endIndex < input.length() && input.charAt(endIndex) != ' '){
+					endIndex++;
+				}
+				String reverse = reverse(input.substring(i,endIndex));
+				i = endIndex-1;		
+				sb.append(reverse);
+			}
+		}
+		return sb.toString();		
+	}
+	
+	public static String reverse(String input){
+		StringBuilder sb = new StringBuilder();
+		for(int i = input.length()-1; i >= 0; i--){
+			sb.append(input.charAt(i));
+		}
+		return sb.toString();
+	}
+
+}
